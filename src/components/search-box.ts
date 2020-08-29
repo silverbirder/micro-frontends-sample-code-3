3981cafee045ed1932e6bc58b34f1c50fd509e0c
shibamoto <silverbirder@gmail.com>
2020-08-29 09:23:19 +0900

    🐯 Dispatch custom event

--- src/components/search-box.ts
+++ src/components/search-box.ts
@@ -1,11 +1,5 @@
-import {LitElement, html, customElement, css} from 'lit-element';
+import {LitElement, html, customElement, css, property, eventOptions, query} from 'lit-element';
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement('search-box')
 export class SearchBox extends LitElement {
     static styles = css`
@@ -16,14 +10,28 @@ export class SearchBox extends LitElement {
       max-width: 800px;
     }
   `;
+    @property({type: String})
+    keyword = '';
 
     render() {
         return html`
-　　  <input type="text"><button @click="${this._onClick}">検索</button>
+　　  <input type="text" id="keyword" @change="${this._onChange}" value="${this.keyword}"><button @click="${this._onClick}">検索</button>
     `;
     }
 
+    @eventOptions({capture: true})
     private _onClick() {
+        let event = new CustomEvent('search-box-button-click', {
+            detail: {
+                keyword: this.keyword
+            }
+        });
+        this.dispatchEvent(event);
+    }
+
+    @eventOptions({capture: true})
+    private _onChange(e: Event) {
+        this.keyword = (<HTMLInputElement>e.target).value;
     }
 }
 
